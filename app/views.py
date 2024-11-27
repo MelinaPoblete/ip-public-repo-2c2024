d892fdfbed84dfbd9c7211c1f6390bd980740897
unknown <pobletemelina2002@gmail.com>
2024-11-27 00:59:38 -0300

    modif

--- app/views.py
+++ app/views.py
@@ -11,11 +11,12 @@ def index_page(request):
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
+    images = getAllImages(input)
     favourite_list = []
-    for i in images:
-        if images[i]==request:
-            print(images[i])
+    return render(request, 'home.html', {
+        'images': images,
+        'favourite_list': favourite_list
+    })
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
 def search(request):
